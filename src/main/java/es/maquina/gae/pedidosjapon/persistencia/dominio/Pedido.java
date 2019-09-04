1941c8ecbb588b5f6ce9bc93cfb5bf502296c551
MaQuiNa1995 <maquina1995@gmail.com>
2019-09-04 20:53:28 +0200

    indexados de entidad y imagen para que no de excepciones random el servlet

--- src/main/java/es/maquina/gae/pedidosjapon/persistencia/dominio/Pedido.java
+++ src/main/java/es/maquina/gae/pedidosjapon/persistencia/dominio/Pedido.java
@@ -1,18 +1,3 @@
-/*
- * Copyright 2016 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package es.maquina.gae.pedidosjapon.persistencia.dominio;
 
 import java.io.Serializable;
@@ -20,6 +5,7 @@ import java.io.Serializable;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.googlecode.objectify.annotation.Entity;
 import com.googlecode.objectify.annotation.Id;
+import com.googlecode.objectify.annotation.Index;
 
 @Entity
 public class Pedido implements Persistible<Long>, Serializable {
@@ -32,7 +18,9 @@ public class Pedido implements Persistible<Long>, Serializable {
 	@Id
 	@JsonIgnore
 	private Long id;
+	@Index
 	private String nombrePedido;
+	@Index
 	private String solicitante;
 	private Double importeMaximo = 0D;
 	private Boolean pagado = Boolean.FALSE;
